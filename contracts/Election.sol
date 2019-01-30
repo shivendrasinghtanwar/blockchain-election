5abafa43e6b847a6c40233db862fbbb748759671
Shivendra <shivendra@ipropal.com>
2019-01-30 16:05:56 +0530

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract Election{
+
+	//Write variable
+
+
+	//Read variable
+	string public candidate;
+
+	//Constructor
+	constructor() public{
+		candidate = "sunny";
+	}
+
+}
