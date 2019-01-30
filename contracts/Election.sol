7ae9f23f3934079b8afb27bc8eb3b0e3420eafaf
Shivendra <shivendra@ipropal.com>
2019-01-30 19:12:40 +0530

    Some changes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,15 +2,25 @@ pragma solidity ^0.5.0;
 
 contract Election{
 
-	//Write variable
-
-
-	//Read variable
-	string public candidate;
-
+	//Model a candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	//Store Candidates
+	//Fetch Candidates
+	mapping(uint=>Candidate) public candidates;
+	//Store Candidates Count
+	uint public candidateCount;
 	//Constructor
 	constructor() public{
-		candidate = "sunny";
+		addCandidate("Sunny Singh");
+		addCandidate("Bunny Singh");
 	}
 
+	function addCandidate(string _name) private{
+		candidateCount++;
+		candidates[candidateCount] = Candidate(candidateCount,_name,0);
+	}
 }
